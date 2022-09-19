7c9d428efe497f3a5a2afe9ee33a3e148ea5d8da
Tristan Alie <TristanAlie@yahoo.com>
2022-09-19 11:42:18 -0400

    more types

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 "use strict";
 exports.__esModule = true;
-console.log('hello world');
+// console.log('hello world')
 function jklol(str) {
     return str + '...jk lol';
 }
-console.log(jklol('fuck you mom'));
+// console.log(jklol('fuck you mom'))
 var num = 86; // any allows us to bypass typescript's rule for types -- try to avoid it though
 num = 'fuckyou';
 var whatever; // if you don't set a value when declaring a variable, its type can be anything
@@ -18,7 +18,7 @@ var me = {
     age: 32,
     favoriteQuote: 'the cake is a lie'
 };
-console.log(me);
+// console.log(me)
 function addAll(a, b, c) {
     var sum = a + b;
     if (c) {
@@ -41,13 +41,16 @@ function changeNumber(newNum) {
     number = newNum;
     console.log(number);
 }
+function screwUp(word) {
+    return;
+}
 var numArr = []; // it's an array that can only be typed with numbers
 var personArr = []; // an array of person objects
 var checkThisShit = [me, 2.5, 'inches thick']; // this follows the OddlySpecific type
 numArr.push(12);
 // numArr.push('hello')
 // numArr.push(true)
-console.log(addAll(1, 2));
+// console.log(addAll(1, 2))
 function helloWorldXTimes(num) {
     var fullString = '';
     for (var i = 0; i < num; i++) {
@@ -72,8 +75,54 @@ var thirdGuy = {
 var activities;
 activities = ['Jump', "Action", /*45*/]; // if I want to put a number into an array of strings, i would have to declare the return value as any[] -- again, any might defeat the purpose of using typescript
 // console.log(otherGuy.name) // because I declared that the otherGuy simply returns an object, it doesn't recognize the property of the object...
-console.log(thirdGuy.name);
+// console.log(thirdGuy.name)
 for (var _i = 0, _a = thirdGuy.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase()); // typescript doesn't complain about string methods in this loop because it knows that the hobbies are all strings
 }
+var newTuple = [5, 'strips of bacon']; // can create arrays with specific values, btw a tuple is an array with a set amount of indices and set types for those indices
+// newTuple[1] = 5 the second index has to be a string
+newTuple.push('pancakes'); // push can add indices to tuples
+//newTuple = [10, 'pancakes', bacon] // but you can't set the tuple to be more values that originally designated
+var meals;
+(function (meals) {
+    meals[meals["BREAKFAST"] = 0] = "BREAKFAST";
+    meals[meals["LUNCH"] = 1] = "LUNCH";
+    meals[meals["DINNER"] = 2] = "DINNER";
+})(meals || (meals = {})); // not in javascript but in many other languages -- creates options as human readable names instead of just doing "1, 2, 3" or something -- all caps is sometimes a naming convention, but not always -- can also set a specific value to each name if you want to
+function eat(meal) {
+    if (meal === meals.BREAKFAST)
+        return 'scrambled eggs';
+    else if (meal === meals.LUNCH)
+        return 'leftovers';
+    else if (meal === meals.DINNER)
+        return 'pork and beans';
+}
+// console.log(eat(meals.BREAKFAST))
+function combine(x, y) {
+    var result;
+    if (typeof x === 'number' && typeof y === 'number') {
+        result = x + y;
+    }
+    else {
+        result = x.toString() + y.toString();
+    }
+    return result;
+}
+var doSomething; // the Function type must be capitalized to work
+var doSomethingSpecific; // set the return value to be a function that takes two numbers and returns a number. "() => someValueType" is how to declare a specific type of function to be returned. if the function always returns a number, you could just set the expected return value to be number and it will work, however if you set a number that isn't created by a function as the return value it would not work with doSomethingSpecific
+function nullCb(a, b, cb) {
+    var result = a + b;
+    cb(result);
+}
+function consoleResult(num) {
+    console.log(num);
+    return num;
+}
+nullCb(25, 3, consoleResult); // the cb in nullCb should return void, but consoleResult should return a number. nullCb is saying that no matter what cb returns it will ignore the return value; so cb still returns void even though the function passed in returns something
+var wtf; // unknown is if the program doesn't yet know what the return type will be
+wtf = 50;
+wtf = function (num) { return num; };
+wtf = 'fuckyou';
+var name;
+// name = wtf // "unknown" is different from "any" in that
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import * as _ from 'lodash'; //
-console.log('hello world')
+// console.log('hello world')
 
 function jklol (str: string): string {
     return str + '...jk lol'
@@ -7,7 +7,7 @@ function jklol (str: string): string {
 
 
 
-console.log(jklol('fuck you mom'))
+// console.log(jklol('fuck you mom'))
 
 let num: any = 86; // any allows us to bypass typescript's rule for types -- try to avoid it though
 
@@ -18,7 +18,7 @@ let whatever; // if you don't set a value when declaring a variable, its type ca
 whatever = 'hello world'
 whatever = 72
 
-type Dumbass = 'asshole' | 'jerk' // Dumbass can only be set to those values 
+type Dumbass = 'asshole' | 'jerk' // Dumbass can only be set to those values -- useful when multiple inputs should only take certain arguments; in js, functions might need complex if statements to reject inputs that don't work
 
 let dumbass: Dumbass ;
 dumbass = 'asshole' // will work
@@ -39,7 +39,7 @@ const me: Person = {
     favoriteQuote: 'the cake is a lie'
 }
 
-console.log(me)
+// console.log(me)
 
 function addAll(a: number, b: number, c?: number): number { // "?" denotes an optional parameter
     let sum = a + b 
@@ -67,6 +67,9 @@ function changeNumber (newNum: number) : void { // void is if you don't return a
     number = newNum
     console.log(number)
 }
+function screwUp (word: object) : undefined { // undefined is a type that can be expected as a return value. i currently don't know how this would be useful
+    return
+}
 
 const numArr: number[] = [] // it's an array that can only be typed with numbers
 const personArr: Person[] = [] // an array of person objects
@@ -76,7 +79,7 @@ const checkThisShit: OddlySpecific = [me, 2.5, 'inches thick'] // this follows t
 numArr.push(12)
 // numArr.push('hello')
 // numArr.push(true)
-console.log(addAll(1, 2))
+// console.log(addAll(1, 2))
 
 function helloWorldXTimes (num: number): string {
     let fullString = ''
@@ -109,14 +112,52 @@ let activities : string[];
 activities = ['Jump', "Action", /*45*/] // if I want to put a number into an array of strings, i would have to declare the return value as any[] -- again, any might defeat the purpose of using typescript
 
 // console.log(otherGuy.name) // because I declared that the otherGuy simply returns an object, it doesn't recognize the property of the object...
-console.log(thirdGuy.name)
+// console.log(thirdGuy.name)
 
 for(let hobby of thirdGuy.hobbies) {
     console.log(hobby.toUpperCase()) // typescript doesn't complain about string methods in this loop because it knows that the hobbies are all strings
 }
 
-let newTuple : [number, string] = [5, 'strips of bacon'] // can create arrays with specific values, btw a tuple is an array with a set amount of indices
+let newTuple : [number, string] = [5, 'strips of bacon'] // can create arrays with specific values, btw a tuple is an array with a set amount of indices and set types for those indices
 // newTuple[1] = 5 the second index has to be a string
 newTuple.push('pancakes') // push can add indices to tuples
 //newTuple = [10, 'pancakes', bacon] // but you can't set the tuple to be more values that originally designated
 
+enum meals { BREAKFAST, LUNCH, DINNER } // not in javascript but in many other languages -- creates options as human readable names instead of just doing "1, 2, 3" or something -- all caps is sometimes a naming convention, but not always -- can also set a specific value to each name if you want to
+
+function eat (meal: meals): string {
+    if(meal === meals.BREAKFAST) return 'scrambled eggs'
+    else if(meal === meals.LUNCH) return 'leftovers'
+    else if(meal === meals.DINNER) return 'pork and beans'
+}
+
+// console.log(eat(meals.BREAKFAST))
+
+function combine(x: string | number, y: string | number): string | number { // "|" is a union, which means more than one type can be expected -- can do more than two with additional unions e.g. "string | number | boolean" etc.
+    let result: string | number 
+    if(typeof x === 'number' && typeof y === 'number') {
+        result = x + y
+    } else {
+        result = x.toString() + y.toString()
+    }
+    return result
+}
+
+let doSomething : Function // the Function type must be capitalized to work
+let doSomethingSpecific : (a: number, b: number) => number // set the return value to be a function that takes two numbers and returns a number. "() => someValueType" is how to declare a specific type of function to be returned. if the function always returns a number, you could just set the expected return value to be number and it will work, however if you set a number that isn't created by a function as the return value it would not work with doSomethingSpecific
+function nullCb(a: number, b: number, cb: (num: number) => void) {
+    let result = a + b
+    cb(result)
+}
+function consoleResult(num: number) {
+    console.log(num)
+    return num
+}
+nullCb(25, 3, consoleResult) // the cb in nullCb should return void, but consoleResult should return a number. nullCb is saying that no matter what cb returns it will ignore the return value; so cb still returns void even though the function passed in returns something
+
+let wtf : unknown // unknown is if the program doesn't yet know what the return type will be
+wtf = 50
+wtf = (num) => num
+wtf = 'fuckyou'
+let name : string
+// name = wtf // "unknown" is different from "any" in that
